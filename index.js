1a2ff103401c6780b52eaca9ab4a640b9771b26a
Magna Dutra <magdutra.jobs@gmail.com>
2021-11-01 10:57:50 -0300

    Updates

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import cipher from './cipher.js';
 
-//console.log(cipher); /* O DOM SERÁ APLICADO AQUI*/
-
 const botao1 = document.getElementById("botao1");
 
 function criptarMensagem(){
-    let chaveDesloc = Number(document.getElementById("chaveDesloc").value); //RESGATANDO O VALOR DO id CHAVE DE DESLOCAMENTO TRANSFORMANDO EM NUMERO
-    let cifrarTxt = document.getElementById("caixa-de-texto-cifrar").value; //CIFRAR LETRAS MAIUSCULAS
-    const cifrando = cipher.encode(chaveDesloc, cifrarTxt); //FORMULA DO DANIEL
-    document.getElementById("caixa-de-texto-decifrar").value = cifrando; //RESULTADO DA FORMULA
+    let chaveDesloc = Number(document.getElementById("chaveDesloc").value); 
+    const cifrando = cipher.encode(chaveDesloc, cifrarTxt); 
+    document.getElementById("caixa-de-texto-decifrar").value = cifrando; 
 }
 
 botao1.addEventListener("click", criptarMensagem);
 
-const botao2 = document.getElementById("botao2"); //DAR A FUNÇÃO DE CLICAR
+const botao2 = document.getElementById("botao2"); 
 
-function decifrarMensagem(){ //SÓ MUDA OS NOMES DAS VARIAVEIS
+function decifrarMensagem(){ 
     let chaveDesloc = Number(document.getElementById("chaveDesloc").value);
     let decifrarTxt = document.getElementById("caixa-de-texto-decifrar").value;
     const decifrando = cipher.decode(chaveDesloc, decifrarTxt);
